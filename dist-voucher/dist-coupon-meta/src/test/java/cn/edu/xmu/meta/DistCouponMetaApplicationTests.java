49a13cdc3037e640c954f6b03e1c3d13f7d95e45
mokeeqian <mokeeqian@gmail.com>
2022-08-23 12:20:09 +0800

    拆分优惠券为模板管理、领券两个微服务

---
+++ dist-voucher/dist-coupon-meta/src/test/java/cn/edu/xmu/meta/DistCouponMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.meta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistCouponMetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ dist-voucher/dist-coupon-record/src/main/java/cn/edu/xmu/record/DistCouponRecordApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistCouponRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistCouponRecordApplication.class, args);
+    }
+
+}
